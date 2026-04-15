3c5dc278922699f93de3d8aa85eb7603d7f6309c
girish9046b <90566230+girish9046b@users.noreply.github.com>
2026-04-15 12:49:41 +0530

    JUNE

--- src/main/java/com/example/demo/BbbApplication.java
+++ src/main/java/com/example/demo/BbbApplication.java
@@ -8,7 +8,7 @@ public class BbbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BbbApplication.class, args);
-		System.out.println("..............test project..commit.APRIL AND MAY......");
+		System.out.println("..............test project..commit.APRIL AND JUNE......");
 	}
 
 }
