6d3b089fcf92bbb63a992f31942e7a5b9ccd3274
girish9046b <90566230+girish9046b@users.noreply.github.com>
2026-04-15 13:05:35 +0530

    changes for june

---
+++ src/main/java/com/example/demo/BbbApplication2.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbbApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbbApplication2.class, args);
+		System.out.println("..............test project..commit.APRIL AND JUNE......");
+	}
+
+}
---
+++ src/main/java/com/example/demo/BbbApplication3.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbbApplication3 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbbApplication3.class, args);
+		System.out.println("..............test project..commit.APRIL AND JUNE......");
+	}
+
+}
