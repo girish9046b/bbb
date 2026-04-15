709c9f430dc31767f1c3a787bfacbc2a21c2ad72
girish9046b <90566230+girish9046b@users.noreply.github.com>
2026-04-15 12:25:57 +0530

    sysy

--- src/main/java/com/example/demo/BbbApplication.java
+++ src/main/java/com/example/demo/BbbApplication.java
@@ -8,6 +8,7 @@ public class BbbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BbbApplication.class, args);
+		System.out.println("..............test project..commit.......");
 	}
 
 }
